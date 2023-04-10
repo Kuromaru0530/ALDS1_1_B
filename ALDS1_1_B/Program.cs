76be76053679b9a33c903131a224c33197e13847
Kuromaru0530 <kuromary77@gmail.com>
2023-04-10 16:39:01 +0900

    プロジェクト ファイルを追加します。

---
+++ ALDS1_1_B/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main()
+    {
+        var Input = Console.ReadLine().Split(' ');
+        var x = int.Parse(Input[0]);
+        var y = int.Parse(Input[1]);
+
+        if (x < y)
+        {
+            (x, y) = (y, x);
+        }
+
+        while (y > 0)
+        {
+            int r = x % y;
+            x = y;
+            y = r;
+        }
+        Console.WriteLine(x);
+    }
+}
